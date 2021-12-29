ed7c932e3a2e6c17c8bc09bb71f875413b5d2d9b
Heinke Schumacher <heinke.schumacher@sub.uni-hamburg.de>
2021-12-29 18:04:21 +0100

    Update Jupyter Notebooks

---
+++ Modul_1/ipynb_basics/py_basics/first_script.py
@@ -0,0 +1,9 @@
+# -*- impding: utf-8 -*-
+
+
+
+name = "Lovelace"
+age = "37"
+
+print(name)
+print(age)
---
+++ Modul_1/ipynb_basics/py_basics/hello.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 27 22:32:00 2021
+
+@author: schumahe
+"""
+# Demo file for Spyder Tutorial
+# Hans Fangohr, University of Southampton, UK
+
+def hello():
+    print("Hello World")
+
+# Main program starts here
+hello()
+    
+
+
